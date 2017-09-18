221207fff845c5a0556c77bd0205cec597b8eb2f
kenNg1 <kng1230@hotmail.com>
2017-09-18 15:16:29 +0800

    transitioned from rails to node

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const usersController = require('../controllers').users;
 const eventsController = require('../controllers').events;
 const detailsController = require('../controllers').details;
 const sportsController = require('../controllers').sports;
+const districtsController = require('../controllers').districts;
 const auth = require('../config/auth');
 
 module.exports = (app) => {
@@ -23,7 +24,7 @@ module.exports = (app) => {
 	// is this necessary?? app.get('/signout', auth.IsAuthenticated, auth.destroySession);
 	// probs don't need profile api as we get the details another way... app.get('/api/profile/:userId', usersController.profile);
 
-	// All events Route
+	// All events routes
 	// fix the , auth.IsAuthenticated , bit
 	app.post('/api/events/', eventsController.create); // new post route
 	app.put('/api/events/:eventId', eventsController.update); // update post route
@@ -31,7 +32,7 @@ module.exports = (app) => {
 	app.get('/api/events/', eventsController.queryIndex, eventsController.index); // all post list route
 	app.get('/api/events/:eventId', eventsController.show); // get post content by id route
 
-	// All details Route
+	// All details routes
 	// fix the , auth.IsAuthenticated , bit
 	app.post('/api/details/', detailsController.create); // new post route
 	app.put('/api/details/:detailId', detailsController.update); // update post route
@@ -39,13 +40,21 @@ module.exports = (app) => {
 	app.get('/api/details/', detailsController.index); // all post list route
 	app.get('/api/details/:detailId', detailsController.show); // get post content by id route
 
-	// All details Route
+	// All sports routes
 	// fix the , auth.IsAuthenticated , bit
 	app.post('/api/sports/', sportsController.create); // new post route
 	app.put('/api/sports/:sportId', sportsController.update); // update post route
 	app.delete('/api/sports/:sportId', sportsController.destroy); // delete post route
 	app.get('/api/sports/', sportsController.index); // all post list route
 	app.get('/api/sports/:sportId', sportsController.show); // get post content by id route
+
+	// All districts routes
+	// fix the , auth.IsAuthenticated , bit
+	app.post('/api/districts/', districtsController.create); // new post route
+	app.put('/api/districts/:districtId', districtsController.update); // update post route
+	app.delete('/api/districts/:districtId', districtsController.destroy); // delete post route
+	app.get('/api/districts/', districtsController.index); // all post list route
+	app.get('/api/districts/:districtId', districtsController.show); // get post content by id route
 
 	// All post Route
 	// app.post('/api/post/', auth.IsAuthenticated, postsController.create); // new post route
